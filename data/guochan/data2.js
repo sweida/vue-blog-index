b057bf93e9c06a66fc2790c011e0759fc23d25ea
sweida <weidaxyy@163.com>
2018-09-11 20:03:12 +0800

    data

--- data/guochan/data2.js
+++ data/guochan/data2.js
@@ -96,25 +96,25 @@ export default [
     "video": "https://play.cdmbo.com/20180904/MWOqUawt/index.m3u8"
   },
   {
-    'id': '10920',
-    'image': '/d/file/guochan/2018-09-04/d3191b681c2e1d79550c04eb3014fb44.jpg',
-    'creatDate': '2018-09-05',
-    'longTime': ' 00:29:04 ',
-    'title': 'XH98HX新作之高跟長腿超性感民國美少女',
-    'video': 'https://play.cdmbo.com/20180903/ioFxWcUA/index.m3u8'
+    "id": "10920",
+    "image": "/d/file/guochan/2018-09-04/d3191b681c2e1d79550c04eb3014fb44.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": " 00:29:04 ",
+    "title": "XH98HX新作之高跟長腿超性感民國美少女",
+    "video": "https://play.cdmbo.com/20180903/ioFxWcUA/index.m3u8"
   }, {
-    'id': '10886',
-    'image': '/d/file/guochan/2018-09-03/2843dc2979c385d8c0a86e791efe2cb1.jpg',
-    'creatDate': '2018-09-05',
-    'longTime': '00:26:42 ',
-    'title': 'XH98HX新作之高跟制服大三學妹誘惑',
-    'video': 'https://play.cdmbo.com/20180902/bHtuCtnu/index.m3u8'
+    "id": "10886",
+    "image": "/d/file/guochan/2018-09-03/2843dc2979c385d8c0a86e791efe2cb1.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:26:42 ",
+    "title": "XH98HX新作之高跟制服大三學妹誘惑",
+    "video": "https://play.cdmbo.com/20180902/bHtuCtnu/index.m3u8"
   }, {
-    'id': '10863',
-    'image': '/d/file/guochan/2018-09-02/7e103cfd29721bea1e2661c54e1b5206.jpg',
-    'creatDate': '2018-09-05',
-    'longTime': '00:16:23 ',
-    'title': '约操黑丝美腿大学生静静 极品小嫩逼毛少水多 无套骑着操 高清720P完整版',
-    'video': 'https://play.cdmbo.com/20180901/AAgy71en/index.m3u8'
+    "id": "10863",
+    "image": "/d/file/guochan/2018-09-02/7e103cfd29721bea1e2661c54e1b5206.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:16:23 ",
+    "title": "约操黑丝美腿大学生静静 极品小嫩逼毛少水多 无套骑着操 高清720P完整版",
+    "video": "https://play.cdmbo.com/20180901/AAgy71en/index.m3u8"
   }
 ]
---
+++ data/porn91/data3.js
@@ -0,0 +1,337 @@
+export default [
+  {
+    "creatDate": "2018-09-09",
+    "id": "11138",
+    "image": "/d/file/gcjp/91porn/2018-09-09/268e17b6894aaf595c0da4c93b9f8f41.gif",
+    "longTime": "00:14:36",
+    "title": "boss哥未公开视频 牛奶浴油亮大屁股美美哒720P完整版",
+    "video": "https://play.cdmbo.com/20180908/RebWBgVI/index.m3u8"
+  },
+  {
+    "creatDate": "2018-09-09",
+    "id": "11137",
+    "image": "/d/file/gcjp/91porn/2018-09-09/a86438b9bf8d18d42e5be401bce008f7.jpg",
+    "longTime": "00:23:21",
+    "title": "爆草黑丝人民女教师 户外口交床上爆草浴室边冲边操精彩剪辑",
+    "video": "https://play.cdmbo.com/20180908/arjSVf8z/index.m3u8"
+  },
+  {
+    "creatDate": "2018-09-08",
+    "id": "11098",
+    "image": "/d/file/gcjp/91porn/2018-09-08/731728b67ec0f8199d55d3cfa5bf49ae.jpg",
+    "longTime": "00:37:24",
+    "title": "91小哥佩奇最新精品第四部18岁外表小清新美女突然来学校请我吃饭看完电影",
+    "video": "https://play.cdmbo.com/20180907/FYGacfhV/index.m3u8"
+  },
+  {
+    "creatDate": "2018-09-05",
+    "id": "10959",
+    "image": "/d/file/gcjp/91porn/2018-09-05/a5cb7beb20800de06a21faa84c1b49c5.jpg",
+    "longTime": "00:22:40",
+    "title": "公司美女会计少妇是老板情人 趁她老公不在去偷情全程自拍杆露脸自拍活儿太好了那叫一个骚_",
+    "video": "https://play.cdmbo.com/20180904/8PAvsxwA/index.m3u8"
+  },
+  {
+    "id": "9381",
+    "image": "/d/file/gcjp/91porn/2018-07-24/2088ffd0cc5c4246a1bbbd60b883e6c8.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:12:56",
+    "title": "风吟鸟唱最近很火的摄影大神用假屌玩操无毛逼模特樊薪朵看表情好像是给玩出高潮了1080P超清",
+    "video": "https://play.cdmbo.com/20180723/R9fGlO1A/index.m3u8"
+  },{
+    "id": "9383",
+    "image": "/d/file/gcjp/91porn/2018-07-24/64351cf252dd03fa80d7a335332a78a2.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "01:06:00",
+    "title": "BOSS哥巨作之蜜臀鱼网袜三人行 疯狂一小时超长完整版",
+    "video": "https://play.cdmbo.com/20180723/15nP1UPW/index.m3u8"
+  },{
+    "id": "9356",
+    "image": "/d/file/gcjp/91porn/2018-07-23/11a569193d3e06c1b951ca89896aac76.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:31:25",
+    "title": "天堂原创素人约啪第10季街头搭讪小麦色性感小野猫上1080P高清完整版",
+    "video": "https://play.cdmbo.com/20180722/vVfRS87b/index.m3u8"
+  },{
+    "id": "9371",
+    "image": "/d/file/gcjp/91porn/2018-07-23/725cf96b0ab425fff177804630ce0d4b.gif",
+    "creatDate": "2018-09-05",
+    "longTime": "00:31:19",
+    "title": "花式调教罗大大玩的很嗨",
+    "video": "https://play.cdmbo.com/20180722/tOA6Or1D/index.m3u8"
+  },{
+    "id": "9357",
+    "image": "/d/file/gcjp/91porn/2018-07-23/71d2bd77506c4f3b4e76881b4bceda6b.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "01:02:10",
+    "title": "风月海棠哥最新出品世界杯助威法国之足球宝贝小汤唯1080P高清完整版",
+    "video": "https://play.cdmbo.com/20180722/UtbdHVNA/index.m3u8"
+  },{
+    "id": "9286",
+    "image": "/d/file/gcjp/91porn/2018-07-21/551e13edc8e80664acdffaabe3d7f145.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:17:16",
+    "title": "仁哥新作身高177兼职车模口爆720P清晰完整原版",
+    "video": "https://play.cdmbo.com/20180720/50xCmxmb/index.m3u8"
+  },{
+    "id": "9246",
+    "image": "/d/file/gcjp/91porn/2018-07-20/7fdf123aae90d4b8a5374800c38f0931.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:27:58",
+    "title": "绝色美女超大奶子真的让我欲罢不能连着干_",
+    "video": "https://play.cdmbo.com/20180719/um7VKlYM/index.m3u8"
+  },{
+    "id": "9219",
+    "image": "/d/file/gcjp/91porn/2018-07-19/9f349d2b626a355605c6a17ba88ca7ae.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:29:20",
+    "title": "天堂素人系列 搭讪过来的妹子酒店开房干炮 人不可貌相 没想到这么淫荡",
+    "video": "https://play.cdmbo.com/20180718/Wh2lq09H/index.m3u8"
+  },{
+    "id": "9186",
+    "image": "/d/file/gcjp/91porn/2018-07-18/e42a899573f6eaf7d96bad987392a1b3.gif",
+    "creatDate": "2018-09-05",
+    "longTime": "00:10:54",
+    "title": "苏州猛男新作 爆虐淫操极品情趣黑丝爆乳女友 床上操到客厅 高清1080P完整版",
+    "video": "https://play.cdmbo.com/20180717/CzRAlZCa/index.m3u8"
+  },{
+    "id": "9182",
+    "image": "/d/file/gcjp/91porn/2018-07-18/fb3e0d39472ca6036c982676ad7c6e69.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:37:23",
+    "title": "小哥佩奇 Academi外表小清新 欲女 1080p高清",
+    "video": "https://play.cdmbo.com/20180717/edRf3p2a/index.m3u8"
+  },{
+    "id": "9153",
+    "image": "/d/file/gcjp/91porn/2018-07-17/8644e0296f2d1234d65079c08389c296.gif",
+    "creatDate": "2018-09-05",
+    "longTime": "00:08:36",
+    "title": "苏州猛男新作 出差前舍不得大屌男友 内裤来不及脱主动骑坐鸡巴上不停扭动 顶操爆乳G奶女友 高清1080P版",
+    "video": "https://play.cdmbo.com/20180716/7BfalRC0/index.m3u8"
+  },{
+    "id": "9151",
+    "image": "/d/file/gcjp/91porn/2018-07-17/8ed9fa68b19be974ea6d56970a6f6abe.gif",
+    "creatDate": "2018-09-05",
+    "longTime": "00:12:21",
+    "title": "自拍达人仁哥首次无码露脸啪啪体育特长生1080P高清无水印原版",
+    "video": "https://play.cdmbo.com/20180716/QUaUQzhr/index.m3u8"
+  },{
+    "id": "8952",
+    "image": "/d/file/gcjp/91porn/2018-07-12/0903232faae38f1675ecf847c0b3a3e3.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:41:26",
+    "title": "汤先生 奇迹的东欧美女",
+    "video": "https://play.cdmbo.com/20180711/xwWAyo4W/index.m3u8"
+  },{
+    "id": "8916",
+    "image": "/d/file/gcjp/91porn/2018-07-11/bf71cda7c7d28e12885f8fbef748cd47.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:39:26",
+    "title": "xxx最新第16部-捆绑道具续战E奶女神宝儿SM潮喷中出高清无水",
+    "video": "https://play.cdmbo.com/20180710/9KopQMYm/index.m3u8"
+  },{
+    "id": "8876",
+    "image": "/d/file/gcjp/91porn/2018-07-10/2994f5bf033a6cffbfd8cb619544c17e.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:13:29",
+    "title": "推倒尽情抽插穿着黑丝直接在沙发勾引美团外卖小哥上吹硬鸡巴再坐上来疯狂扭动",
+    "video": "https://play.cdmbo.com/20180709/YXmC5oaI/index.m3u8"
+  },{
+    "id": "8887",
+    "image": "/d/file/gcjp/91porn/2018-07-10/f649aa238a12ab704043494f548cfbc2.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:38:38",
+    "title": "爱丝小仙女思妍最新作品-性感170CM模特灰丝秀-1080p高清完整版",
+    "video": "https://play.cdmbo.com/20180709/FcdZtc48/index.m3u8"
+  },{
+    "id": "8858",
+    "image": "/d/file/gcjp/91porn/2018-07-09/fab379b371e8e35d3cd3aa40ffded4b4.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:47:10",
+    "title": "月海棠豪华出品 极品黑丝制服第一蜜桃美臀 超清1080P原版完整无水印",
+    "video": "https://play.cdmbo.com/20180708/RtvJP3Mx/index.m3u8"
+  },{
+    "id": "8825",
+    "image": "/d/file/gcjp/91porn/2018-07-08/90052ca08e987bb4e5e948a80c1a2a22.gif",
+    "creatDate": "2018-09-05",
+    "longTime": "00:17:46",
+    "title": "L先生出品 极品白嫩骚浪95年小女友 情趣黑丝小骚货终极调教多姿势无套爆操 高清1080P",
+    "video": "https://play.cdmbo.com/20180707/EiVVHrgy/index.m3u8"
+  },{
+    "id": "8785",
+    "image": "/d/file/gcjp/91porn/2018-07-07/e1ff822be3684be4dabd4884ed6a582a.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:18:53",
+    "title": "小哥佩奇编号003-无死角小仙女Sophia 单身派对认识的女神聊了不到一周出来啪啪",
+    "video": "https://play.cdmbo.com/20180706/fIxfNNtW/index.m3u8"
+  },{
+    "id": "8688",
+    "image": "/d/file/gcjp/91porn/2018-07-05/17db2ce4ad73bc7c2c7207b1aaaab385.jpg",
+    "creatDate": "2018-09-05",
+    "longTime": "00:50:59",
+    "title": "sison街頭搭訕素人第一季超級性感的音樂教師VIP",
+    "video": "https://play.cdmbo.com/20180704/DvdTgiqC/index.m3u8"
+  },
+  {
+    "id": "9696",
+    "image": "/d/file/gcjp/91porn/2018-08-01/fe1bf398afa43256bf66c9a3340f2615.gif",
+    "creatDate": "2018-08-01",
+    "longTime": "00:04:52",
+    "title": "新人巨乳制造者 这大奶子真诱人 乳交都看不到鸡巴 抓着大奶子爆草 真会享受",
+    "video": "https://play.cdmbo.com/20180731/7eAttLRL/index.m3u8"
+  }, {
+    "id": "9662",
+    "image": "/d/file/gcjp/91porn/2018-07-31/44edf3eee0d9a6ffe193b086ead268cf.jpg",
+    "creatDate": "2018-07-31",
+    "longTime": "00:43:10",
+    "title": "秦先生第五部女神换上了吊带丝袜情趣套装啪啪一整晚720P无水印完整版",
+    "video": "https://play.cdmbo.com/20180730/LthZuXfT/index.m3u8"
+  }, {
+    "id": "9610",
+    "image": "/d/file/gcjp/91porn/2018-07-30/56e6b43042b1eabecdb6cee4917a5ae5.jpg",
+    "creatDate": "2018-07-30",
+    "longTime": "00:29:57",
+    "title": "猴小雪高价出售视频之模特学院系列高清原版",
+    "video": "https://play.cdmbo.com/20180729/v7dkDflB/index.m3u8"
+  }, {
+    "id": "9607",
+    "image": "/d/file/gcjp/91porn/2018-07-30/9d70b42759c781782e1f40f8888c2436.jpg",
+    "creatDate": "2018-07-30",
+    "longTime": "00:14:20",
+    "title": "新疆一匹狼系列之新疆女神跟踪到如意楼道内高清无水印完整版",
+    "video": "https://play.cdmbo.com/20180729/xXuRgJsk/index.m3u8"
+  }, {
+    "id": "9591",
+    "image": "/d/file/gcjp/91porn/2018-07-29/8213a96213847a5da5520647390e26f3.jpg",
+    "creatDate": "2018-07-29",
+    "longTime": "00:40:45",
+    "title": "猛男战斗力强悍高级休闲会所玩双飞姐妹花 推油毒龙啪啪啪",
+    "video": "https://play.cdmbo.com/20180728/qpj1hBx8/index.m3u8"
+  }, {
+    "id": "9590",
+    "image": "/d/file/gcjp/91porn/2018-07-29/1829eb3a9f2d6d3d21784d575400db7e.gif",
+    "creatDate": "2018-07-29",
+    "longTime": "00:11:40",
+    "title": "-我老公出差了 今天不回来 结婚照下狂干刚结婚不久的妹纸",
+    "video": "https://play.cdmbo.com/20180728/O8qcAdaJ/index.m3u8"
+  }, {
+    "id": "9573",
+    "image": "/d/file/gcjp/91porn/2018-07-29/cdbb654adf508b2620e3d8fe0a7c7c22.gif",
+    "creatDate": "2018-07-29",
+    "longTime": "00:35:19",
+    "title": "秦先生陌陌是个很靠谱的软件",
+    "video": "https://play.cdmbo.com/20180728/XDENOcGq/index.m3u8"
+  }, {
+    "id": "9552",
+    "image": "/d/file/gcjp/91porn/2018-07-28/67a1abcd11e1e9d553e989759ddf1e05.jpg",
+    "creatDate": "2018-07-28",
+    "longTime": "00:41:29",
+    "title": "秦先生第14季超美气质嫩模女神高端私人定製720P无水印完整版",
+    "video": "https://play.cdmbo.com/20180727/MglWQB3w/index.m3u8"
+  }, {
+    "id": "9551",
+    "image": "/d/file/gcjp/91porn/2018-07-28/a5066e883227b3da871a03d664487402.jpg",
+    "creatDate": "2018-07-28",
+    "longTime": "00:24:14",
+    "title": "约操某外贸公司性感漂亮女主管 毛少貌美超敏感 刚插进去呻吟大叫 说 射我嘴里 1080P高清完整版",
+    "video": "https://play.cdmbo.com/20180727/F1AnDC10/index.m3u8"
+  }, {
+    "id": "9506",
+    "image": "/d/file/gcjp/91porn/2018-07-27/225288d3609cb1336c1fe5278a14c1e1.jpg",
+    "creatDate": "2018-07-27",
+    "longTime": "00:31:20",
+    "title": "新人小a番号02奶子非常漂亮有个唇印的会所小妹",
+    "video": "https://play.cdmbo.com/20180726/Dv3U9doU/index.m3u8"
+  }, {
+    "id": "9509",
+    "image": "/d/file/gcjp/91porn/2018-07-27/54b936c6b86d4e843b2e0a9f98bdc602.jpg",
+    "creatDate": "2018-07-27",
+    "longTime": "00:31:07",
+    "title": "美容院老板娘搞上瘾了再次约炮这次吃了颗伟哥一晚干了她四次说她没体力了",
+    "video": "https://play.cdmbo.com/20180726/JAynUvlo/index.m3u8"
+  }, {
+    "id": "9475",
+    "image": "/d/file/gcjp/91porn/2018-07-26/67a055ad589fd7fbe058e077de8bab6a.jpg",
+    "creatDate": "2018-07-26",
+    "longTime": "00:05:24",
+    "title": "吐血推荐风吟鸟唱最新出品极品嫩模小巩俐自慰完给摄影师吹箫1080P高清无遮挡原版",
+    "video": "https://play.cdmbo.com/20180725/05TFwWgS/index.m3u8"
+  }, {
+    "id": "9467",
+    "image": "/d/file/gcjp/91porn/2018-07-26/ee0d5f270f6949f9656cc5e01984ca8e.gif",
+    "creatDate": "2018-07-26",
+    "longTime": "00:29:26",
+    "title": "肛交少妇 深喉爆菊1080P高清完整版",
+    "video": "https://play.cdmbo.com/20180725/SAkHmTB6/index.m3u8"
+  }, {
+    "id": "9464",
+    "image": "/d/file/gcjp/91porn/2018-07-26/479c595df8ad0b965558d17b1a1f9954.jpg",
+    "creatDate": "2018-07-26",
+    "longTime": "00:09:14",
+    "title": "极品白嫩S身材披肩美乳奶茶店老板娘 翘白嫩美臀等大J8伺候 业务繁忙接N电话还没射就回去了",
+    "video": "https://play.cdmbo.com/20180725/lmAX2ZpQ/index.m3u8"
+  }, {
+    "id": "9433",
+    "image": "/d/file/gcjp/91porn/2018-07-25/3e4ceaa2adb5a661c876dee1c8cdf83d.gif",
+    "creatDate": "2018-07-25",
+    "longTime": "00:10:20",
+    "title": "苏州猛男新作 极品混血洋女友穿比基尼勾引当然不放过",
+    "video": "https://play.cdmbo.com/20180724/O4znLQdB/index.m3u8"
+  }, {
+    "id": "9762",
+    "image": "/d/file/gcjp/91porn/2018-08-03/b4e9175e69965ede2b6f22bc27903aa6.jpg",
+    "creatDate": "2018-09-04",
+    "longTime": "00:18:30",
+    "title": "小鲜肉约172CM平面模特女神酒店操B 蜂窝翘臀水太多不小心射了",
+    "video": "https://play.cdmbo.com/20180802/hc9SBDHt/index.m3u8"
+  }, {
+    "id": "9735",
+    "image": "/d/file/gcjp/91porn/2018-08-02/920acd40eb1a5147cba51715e2addda2.jpg",
+    "creatDate": "2018-09-04",
+    "longTime": "00:42:40",
+    "title": "引爆网络的经典电动臀拉丁舞老师叫床呻吟特别尖叫骚到不行高清完整版",
+    "video": "https://play.cdmbo.com/20180801/IfOpeBV5/index.m3u8"
+  }, {
+    "id": "10898",
+    "image": "/d/file/gcjp/91porn/2018-09-03/e0b2385a4fc5ad86c54585866cd0331f.jpg",
+    "creatDate": "2018-09-03",
+    "longTime": "00:32:58",
+    "title": "XH98HX新作之極致美女學妹水手服網襪制服誘惑",
+    "video": "https://play.cdmbo.com/20180902/WWW5a0wX/index.m3u8"
+  }, {
+    "id": "10905",
+    "image": "/d/file/gcjp/91porn/2018-09-03/619027570fc7298f98414bb3542cf8cd.jpg",
+    "creatDate": "2018-09-03",
+    "longTime": "00:13:45",
+    "title": "广东小鲜肉约pao极品嫩模YUKI",
+    "video": "https://play.cdmbo.com/20180902/YLT2bvJ0/index.m3u8"
+  }, {
+    "id": "10809",
+    "image": "/d/file/gcjp/91porn/2018-09-01/d9cd10a383d834ccfc6451747e3a7a4f.gif",
+    "creatDate": "2018-09-01",
+    "longTime": "00:17:59",
+    "title": "91新人鼠哥大战36D大胸颜值不错的网红妹子1080P高清原版",
+    "video": "https://play.cdmbo.com/20180831/ZcxikE5Y/index.m3u8"
+  }, {
+    "id": "10796",
+    "image": "/d/file/gcjp/91porn/2018-08-31/30a0cc58e26d33134329d673fb99a3ae.jpg",
+    "creatDate": "2018-08-31",
+    "longTime": "00:41:42",
+    "title": "大黄鸭第十四季性感黑丝OL和睡裙换装艹丝袜恋物癖720P完整版",
+    "video": "https://play.cdmbo.com/20180830/7nZ5GCji/index.m3u8"
+  }, {
+    "id": "10717",
+    "image": "/d/file/gcjp/91porn/2018-08-29/3e7dbfad4db97f53f2f6449091f66896.gif",
+    "creatDate": "2018-08-29",
+    "longTime": "00:10:08",
+    "title": "蛋叔无套操长腿良家少妇 背着老公给我毒龙口爆吞精",
+    "video": "https://play.cdmbo.com/20180828/WoD7AAdG/index.m3u8"
+  }, {
+    "id": "10719",
+    "image": "/d/file/gcjp/91porn/2018-08-29/a08ea1a09be515e1584f2c06a9e22c72.gif",
+    "creatDate": "2018-08-29",
+    "longTime": "00:33:30",
+    "title": "震撼新作 广东饥渴美女和大屌黑人玩3P 淫叫受不了J8太大",
+    "video": "https://play.cdmbo.com/20180828/gZgfZMUw/index.m3u8"
+  }
+]
