2bb536d52ca488c75887893ed8ecb9aedeefbd08
sweida <weidaxyy@163.com>
2021-01-31 17:20:45 +0800

    2

--- src/modules/manage/api/baseUrlConfig.js
+++ src/modules/manage/api/baseUrlConfig.js
@@ -7,19 +7,19 @@ const baseURL = {
   /** 生产 */ 
   production: {
     ...common,
-    host: 'https://cs.creditcard.ecitic.com',
-    auth: "https://creditcard.ecitic.com/citiccard",
-    prefix: "https://cs.creditcard.ecitic.com/citiccard/mgm-platform-ocp/api/partner",
-    channel: 'https://cs.creditcard.ecitic.com/citiccard/mgm-platform-ocp/api/channel'
+    host: '',
+    auth: '',
+    prefix: '',
+    channel: ''
   },
   /** 开发 */
   development: {
     ...common,
-    host: 'https://e.test.bank.ecitic.com',
-    auth: "http://e.test.bank.ecitic.com/citiccard",
-    prefix: "http://localhost:3721/citiccard/mgm-platform-ocp/api/partner",
-    prefix: "https://e.test.bank.ecitic.com/citiccard/mgm-platform-ocp/api/partner",
-    channel: 'https://e.test.bank.ecitic.com/citiccard/mgm-platform-ocp/api/channel',
+    host: '',
+    auth: '',
+    prefix: '',
+    prefix: '',
+    channel: '',
   }
 }[process.env.NODE_ENV || 'production']
 
